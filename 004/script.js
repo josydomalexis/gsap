8bea3eb8a14a1dffaeb90c177971734df863cfd9
josydomalexis <josyanimator@gmail.com>
2023-10-28 22:26:08 +0530

    gsaps scroll trigger plugin updated

--- 004/script.js
+++ 004/script.js
@@ -1,20 +1,53 @@
 let tl = gsap.timeline();
-tl.from("#heading", {
-  y: -50,
-  duration: 0.5,
-  delay: 0,
-  scale: 1,
-  color: "red",
+
+tl.from(".logoImage, .menu a, .menu_CTA a", {
+  duration: 0.4,
+  scale: 1.5,
+  y: -60,
   repeat: 0,
+  opacity: 0,
+  stagger: 0.15,
 });
-tl.from("#box1", {
-  x: window.innerWidth / 2,
-  duration: 2,
-  delay: 0.1,
-  backgroundColor: "red",
-  rotate: 360,
-  scale: 0.0,
-  stagger: 0.5,
-  repeat: 1,
+
+tl.from(".heroText span", {
+  duration: 0.4,
+  scale: 1.5,
+  y: 60,
+  repeat: 0,
+  opacity: 0,
+  stagger: 0.3,
+});
+
+tl.from(".scrollDown", {
+  duration: 0.4,
+  opacity: 0,
+  y: -60,
+});
+
+tl.to(".scrollDown", {
+  duration: 0.4,
+  y: -60,
+  repeat: -1,
   yoyo: true,
 });
+
+gsap.from("#page1 h2, #page1 p", {
+  duration: 0.4,
+  scale: 1.5,
+  y: 60,
+  repeat: 0,
+  opacity: 0,
+  stagger: 0.3,
+  scrollTrigger: {
+    trigger: "#page1 h2",
+    scroller: "body",
+    // markers: true,
+    start: "top 60%",
+    end: "top 10%",
+    scrub: 2,
+  },
+});
+
+let tl2 = timeline();
+
+let scrollDownText = document.querySelector(".scrollDown");
